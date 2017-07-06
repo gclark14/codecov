f015623f298b90767bc39a0f924406372d224086
Gabe Clark <unrivalled.dh@gmail.com>
2017-07-06 09:15:07 -0700

    Threshold lowered to half a percent diff

--- src/main/java/com/example/demo/Subtractor.java
+++ src/main/java/com/example/demo/Subtractor.java
@@ -8,4 +8,6 @@ public class Subtractor {
     int subtract(int a, int b) { return a - b; }
 
     double subtract(double a, double b) { return a - b; }
+
+    double subtract(double a, int b) { return a - b; }
 }
